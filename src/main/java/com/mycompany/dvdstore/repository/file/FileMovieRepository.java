e8a09c2df1386d1b524ae9cc4596349748ea36d2
Ramm759 <fredgsn59@gmail.com>
2023-06-25 21:39:40 +0200

    valorisation propriété dans application.properties

--- src/main/java/com/mycompany/dvdstore/repository/file/FileMovieRepository.java
+++ src/main/java/com/mycompany/dvdstore/repository/file/FileMovieRepository.java
@@ -15,23 +15,17 @@ public class FileMovieRepository implements MovieRepositoryInterface {
     @Value("${movies.file.location}")
     private File file;
 
-    public void add(Movie movie) {
+    public void add(Movie movie){
         FileWriter writer;
-        try {
-            writer = new FileWriter(file, true);
-            writer.write(movie.getTitle() + ";" + movie.getGenre() + "\n");
+        try{
+            writer=new FileWriter(file,true);
+            writer.write(movie.getTitle()+";"+movie.getGenre()+"\n");
             writer.close();
-        } catch (IOException e) {
+        }
+        catch (IOException e){
             e.printStackTrace();
         }
-        System.out.println("The movie " + movie.getTitle() + " has been added.");
+        System.out.println("The movie "+movie.getTitle()+" has been added.");
     }
 
-    public File getFile() {
-        return file;
-    }
-
-    public void setFile(File file) {
-        this.file = file;
-    }
 }
